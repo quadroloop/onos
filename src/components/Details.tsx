bc42d5a275922cb05e7e865e7bc16f7477f38ebd
Bryce Mercines <nplixel@gmail.com>
2020-05-31 03:24:07 +0800

    complete render of initial details card

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -14,59 +14,47 @@ const Details = (props) => {
               <div className="card card-profile shadow1 border-none mb-3">
                 <img src={img_placeholder} alt="Image placeholder" className="card-img-top" />
 
-                <div className="row justify-content-center">
-                  <div className="col-lg-3 order-lg-2">
-                    <div className="card-profile-image">
-                      <a href="#">
-                        <img src="https://avatars1.githubusercontent.com/u/29462205?s=460&u=4509023c1c8946e206197eca17cdd589572249c0&v=4" className="rounded-circle" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                  <div className="d-flex justify-content-between">
-                    <a href="#" className="btn btn-sm btn-info  mr-4 ">Connect</a>
-                    <a href="#" className="btn btn-sm btn-default float-right">Message</a>
-                  </div>
-                </div>
-
-                <div className="card-body pt-0">
-                  <div className="row">
-                    <div className="col">
-                      <div className="card-profile-stats d-flex justify-content-center">
-                        <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
-                        </div>
-                        <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
-                        </div>
-                        <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                {props.incident && (
+                  <>
+                    <div className="row justify-content-center">
+                      <div className="col-lg-3 order-lg-2">
+                        <div className="card-profile-image">
+                          <a href="#">
+                            <img src="https://avatars1.githubusercontent.com/u/29462205?s=460&u=4509023c1c8946e206197eca17cdd589572249c0&v=4" className="rounded-circle" />
+                          </a>
                         </div>
                       </div>
                     </div>
-                  </div>
+
+                    <div className="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                      <div className="d-flex justify-content-between">
+                        <a href="#" className="btn btn-sm btn-info  mr-4 ">Verify</a>
+                        <a href="#" className="btn btn-sm btn-default float-right">Dismiss</a>
+                      </div>
+                    </div>
+                  </>
+                )}
+
+                <div className="card-body pt-3">
+
                   <div className="text-center">
                     <h5 className="h3">
-                      Jessica Jones<span className="font-weight-light">, 27</span>
+                      Bryce Mercines
                     </h5>
                     <div className="h5 font-weight-300">
                       <br />
                       <i className="ni location_pin mr-2" />Bucharest, Romania
                     </div>
                     <div className="h5 mt-4">
-                      <i className="ni business_briefcase-24 mr-2" />Solution Manager - Creative Tim Officer
+                      Lorem Ipsum Dolor simet
                     </div>
                     <div>
-                      <i className="ni education_hat mr-2" />University of Computer Science
+                      Lorem Ipsum Dolor simet
                     </div>
                   </div>
                 </div>
               </div>
+
             </div>
 
 
