49e7143f544060af73ed2c6e2c8ac0887a86f6c1
Bryce Mercines <nplixel@gmail.com>
2020-05-31 18:27:53 +0800

    implemented col adjustment

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import img_placeholder from '../assets/mapx.png'
+import img_placeholder from '../assets/mapb.png'
 import WeatherDetails from './WeatherDetails';
 import CovidDetails from './CovidDetails';
 import LocationDetails from './LocationDetails';
@@ -33,58 +33,61 @@ const Details = (props) => {
       <div className="container">
 
         {/* Page content */}
+
         <div className="container-fluid">
           <h1><i className="la la-bullseye text-danger" /> Incident Report</h1>
           <div className="row">
-            <div className="col-xl-4 order-xl-2">
-              <div className="card card-profile shadow1 border-none mb-3">
-                <img src={img_placeholder} alt="Image placeholder" className="card-img-top" />
 
-                {props.incident && (
-                  <>
-                    <div className="row justify-content-center">
-                      <div className="col-lg-3 order-lg-2">
-                        <div className="card-profile-image">
-                          <a href="#">
-                            <img src="https://avatars1.githubusercontent.com/u/29462205?s=460&u=4509023c1c8946e206197eca17cdd589572249c0&v=4" className="rounded-circle" />
-                          </a>
-                        </div>
+            {localStorage.detailtype === "incident" && (
+              <div className="col-xl-4 order-xl-2 fade-in">
+                <div className="card card-profile shadow1 border-none mb-3">
+                  <img src={img_placeholder} alt="Image placeholder" className="card-img-top" />
+
+
+                  <div className="row justify-content-center">
+                    <div className="col-lg-3 order-lg-2">
+                      <div className="card-profile-image mt--5">
+                        <a href="#">
+                          <img src="https://avatars1.githubusercontent.com/u/29462205?s=460&u=4509023c1c8946e206197eca17cdd589572249c0&v=4" className="rounded-circle" />
+                        </a>
                       </div>
                     </div>
+                  </div>
 
-                    <div className="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                      <div className="d-flex justify-content-between">
-                        <a href="#" className="btn btn-sm btn-info  mr-4 ">Verify</a>
-                        <a href="#" className="btn btn-sm btn-default float-right">Dismiss</a>
-                      </div>
+                  <div className="card-header text-center border-0 pt-2 pt-md-4 pb-0 pb-md-4">
+                    <div className="d-flex justify-content-between">
+                      <a href="#" className="btn btn-sm btn-info  mr-4 ">Verify</a>
+                      <a href="#" className="btn btn-sm btn-default float-right">Dismiss</a>
                     </div>
-                  </>
-                )}
+                  </div>
 
-                <div className="card-body pt-3">
 
-                  <div className="text-center">
-                    <h5 className="h3">
-                      Bryce Mercines
-                    </h5>
-                    <div className="h5 font-weight-300">
-                      <br />
-                      Manila, Philippines
+
+                  <div className="card-body">
+
+                    <div className="text-center mt--5">
+                      <h5 className="h3">
+                        Bryce Mercines
+                      </h5>
+                      <div className="h5 font-weight-300">
+                        <br />
+                        Manila, Philippines
                     </div>
-                    <div className="h5 mt-4">
-                      Lorem Ipsum Dolor simet
+                      <div className="h5 mt-4">
+                        Lorem Ipsum Dolor simet
                     </div>
-                    <div>
-                      Lorem Ipsum Dolor simet
+                      <div>
+                        Lorem Ipsum Dolor simet
+                    </div>
                     </div>
                   </div>
                 </div>
+
               </div>
+            )}
 
-            </div>
 
-
-            <div className="col-xl-8 order-xl-1">
+            <div className={localStorage.detailtype === "incident" ? "col-xl-8 order-xl-1" : "container"}>
 
               <LocationDetails
                 data={location}
@@ -97,6 +100,7 @@ const Details = (props) => {
             </div>
           </div>
         </div>
+
       </div>
     </div>
   )
