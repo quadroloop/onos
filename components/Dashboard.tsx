8e491693896f10152d1b4cb38a109ba9d0e5dc7f
Bryce Mercines <nplixel@gmail.com>
2020-11-02 19:06:47 +0800

    added info bar component

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -7,8 +7,11 @@ const Dashboard = () => {
       <div className="onos-sidebar">
         <img src={"/images/onos.svg"} className="onos-logo" />
       </div>
+      <div className="info-bar">
+        <h1>Hello</h1>
+      </div>
       <div className="image-container">
-        <MapInteractionCSS maxScale={100}>
+        <MapInteractionCSS maxScale={100} minScale={1}>
           <img
             src={
               "https://src.meteopilipinas.gov.ph/repo/mtsat-colored/24hour/latest-him-colored.gif"
