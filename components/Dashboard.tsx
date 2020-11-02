e82aa65ff331e0c34ed24230ea51db74a65b687a
Bryce Mercines <nplixel@gmail.com>
2020-11-02 22:17:19 +0800

    added sidebar navigation components

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,25 +1,37 @@
 import { MapInteractionCSS } from "react-map-interaction";
 import "../styles/dashboard.scss";
+import Layout from "./layout";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-page">
-      <div className="onos-sidebar">
-        <img src={"/images/onos.svg"} className="onos-logo" />
+    <Layout>
+      <div className="dashboard-page">
+        <div className="onos-sidebar">
+          <img src={"/images/onos.svg"} className="onos-logo" />
+
+          <div className="sidebar-nav">
+            <i className="la la-map active" />
+            <i className="la la-sliders" />
+            <i className="la la-line-chart" />
+            <a href="https://github.com/quadroloop/onos" target="_blank">
+              <i className="la la-github" />
+            </a>
+          </div>
+        </div>
+        <div className="info-bar">
+          <h1>Hello</h1>
+        </div>
+        <div className="map-container">
+          <MapInteractionCSS maxScale={100} minScale={1}>
+            <img
+              src={
+                "https://src.meteopilipinas.gov.ph/repo/mtsat-colored/24hour/latest-him-colored.gif"
+              }
+            />
+          </MapInteractionCSS>
+        </div>
       </div>
-      <div className="info-bar">
-        <h1>Hello</h1>
-      </div>
-      <div className="map-container">
-        <MapInteractionCSS maxScale={100} minScale={1}>
-          <img
-            src={
-              "https://src.meteopilipinas.gov.ph/repo/mtsat-colored/24hour/latest-him-colored.gif"
-            }
-          />
-        </MapInteractionCSS>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,66 +1,28 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "./layout.module.css";
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Onos";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div className={styles.container}>
+      <title>Onos</title>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/onos-logo.png" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Philippine Seallite Weather Dashboard"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={`/images/onos-logo.png`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <link
+          rel="stylesheet"
+          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+        ></link>
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
+
+      {children}
     </div>
-  )
+  );
 }
