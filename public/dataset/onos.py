225b1c7e136fa2448e373ccc3ea83b386bdd880c
Bryce Mercines <nplixel@gmail.com>
2020-11-03 11:16:47 +0800

    working image fetch, using url lib, add current weather snapshot

--- public/dataset/onos.py
+++ public/dataset/onos.py
@@ -1,4 +1,20 @@
 import sys
+import urllib.request
+import datetime
+
+args = sys.argv
+
+date_now = datetime.datetime.now()
+
+
+latest_infrared_image = "http://src.meteopilipinas.gov.ph/repo/mtsat-colored/24hour/latest-him-colored.gif"
+
+
+def help():
+  print("Onos CLI is a tool to update content for the web application.")
+  print("\nUsage:\n")
+  print("-s : Create snapshot of the current weather info. \n MSTAT Infrared Satellite images from DOST PAGASA / HIMAWARI-8 Satellite")
+  print("-ds : Document Storm; create documentation of a storm in occurence")
 
 
 def splash():
@@ -6,16 +22,27 @@ def splash():
   print(" ONOS v1 | Content CLI")
   print("==============================")
 
-def help():
-  print("Onos CLI is a tool to update content for the web application.")
-  print("Usage:")
-  print("-s : Create snapshot of the current weather info.")
+  if len(args) == 1:
+    help()
 
+def currentWeatherSnapshot():
+  print("Preparing current weather snapshot...")
+  print(date_now)
+
+  # print('Fetching latest image from: '+ latest_infrared_image)
+  # image_name = "onos-snapshot-"+str(date_now).replace(" ","-")+".gif";
+  # urllib.request.urlretrieve(latest_infrared_image, image_name)
+  # print("snapshot iamge saved!")
+
+# show splash
 splash()
 
-args = sys.argv
-
+# show help after argument provided:
 if "-h" in args:
   help()
+
+# trigger current weather snapshot
+if "-s" in args:
+  currentWeatherSnapshot()
 
 
