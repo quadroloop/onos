c1010125bdaab65e178435e1328def08320681da
Bryce Mercines <nplixel@gmail.com>
2020-11-08 23:30:19 +0800

    minor UI update

--- components/FilterSettings.tsx
+++ components/FilterSettings.tsx
@@ -34,6 +34,22 @@ const FilterSettings = () => {
           />
         </div>
       </div>
+
+      <div className="card-item slider-segment">
+        <div className="filter-sliders">
+          <small>
+            <i className="la la-swatchbook" /> HUE ROTATE
+          </small>
+          <ReactSlider
+            className="onos-slider"
+            thumbClassName="s-thumb"
+            trackClassName="y-track"
+            renderThumb={(props, state) => (
+              <div {...props}>{state.valueNow}</div>
+            )}
+          />
+        </div>
+      </div>
     </>
   );
 };
