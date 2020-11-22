0277840230805585e4243849d728d56798bb80e3
Bryce Mercines <nplixel@gmail.com>
2020-11-23 00:26:41 +0800

    label capitalization fix

--- components/FilterSettings.tsx
+++ components/FilterSettings.tsx
@@ -146,7 +146,7 @@ const FilterSettings = () => {
       <div className="card-item slider-segment">
         <div className="filter-sliders">
           <small>
-            <i className="la la-swatchbook" /> Contrast
+            <i className="la la-swatchbook" /> CONTRAST
           </small>
           <ReactSlider
             className="onos-slider"
